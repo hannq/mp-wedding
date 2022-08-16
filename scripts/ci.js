2250b4ffc17d62159338774567472fcd363ca9a4
hannq <18112417530@163.com>
2022-08-16 11:42:26 +0800

    chore(ci): fix ci mix err

--- scripts/ci.js
+++ scripts/ci.js
@@ -2,10 +2,10 @@
 
 const execa = require('execa');
 const path = require('path');
+const chalk = require('chalk');
 
 ;(async function() {
-  await Promise.all([
-    execa(`npx pnpm run weapp:ci`, { shell: true, cwd: path.join(__dirname, '../client'), stderr: 'inherit', stdout: 'inherit' }),
-    execa(`npx pnpm run ci`, { shell: true, cwd: path.join(__dirname, '../cloud'), stderr: 'inherit', stdout: 'inherit' })
-  ])
+  await execa(`npx pnpm run weapp:ci`, { shell: true, cwd: path.join(__dirname, '../client'), stderr: 'inherit', stdout: 'inherit' }),
+  await execa(`npx pnpm run ci`, { shell: true, cwd: path.join(__dirname, '../cloud'), stderr: 'inherit', stdout: 'inherit' })
+  console.log(chalk.green('🎉 CI Run Successfully !'));
 })();
